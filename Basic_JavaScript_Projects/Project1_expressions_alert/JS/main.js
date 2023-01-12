547999fafc2c65223ee58da923bdfeb68a2d4e6a
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-11 20:01:40 -0700

    first JS effort
    html + js

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+alert("Omalley is coding."); //I can comment my code like this!
+document.write("\"We must believe that we are gifted for something"
++ " and that this thing must be attained.\""
++ "-Marie Curie");
+var A = "Omalley" + " is coding.";
+document.write(A);
+var dog = "Luna", human1 = "Omalley", human2 = "Solomon";
+document.write(dog);
+document.write(human2);
+document.write(human1);
+document.write("4+5="); //If I put quotes around it, it prints string
+document.write(4+5);  //If there are no quotes, it completes the expression
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+
+document.write(Sent1 + Sent2);
+
