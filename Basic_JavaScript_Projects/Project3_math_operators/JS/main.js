d8c5f75bd7afdd5f483cec953249e46399db4fbc
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-12 09:33:00 -0700

    rough attempts
    I don't understand getelementbyid

---
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,9 @@
+function add(x, y)   {   
+      let mathy = x + y;
+      alert(mathy);
+}  
+
+function sub(x, y) {
+    var submathy = x = y;
+    document.getElementById("Math".innerHTML = "5 - 2 =");
+}
