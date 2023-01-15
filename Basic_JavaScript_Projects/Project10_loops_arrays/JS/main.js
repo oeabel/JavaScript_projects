cf2a138a8373e06c2dafb8469a477e48a1545df9
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-15 14:41:37 -0700

    more length
    Still having an issue with length

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -11,3 +11,13 @@ function Call_Loop() {
 let sentence = "Omalley Abel is coding.";
 let slen = sentence.length;
 document.getElementById("lstr").innerHTML = slen;
+
+var vegetables = ["lettuce", "carrots", "tomatoes", "kohlrabi", "beans"];
+var veggies = "";
+var X;
+function for_Loop() {
+    for (X = 0; X < vegetables.length; X++) {
+        veggies += vegetables[X] + "<br>";
+    }
+    document.getElementById("veg_garden").innerHTML = veggies;
+}
