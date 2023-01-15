6afab24951ff41b89daa36666562a0ad9a9120a0
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-14 21:33:42 -0700

    added slideshow
    image slideshow

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,10 +1,10 @@
-function countdown() {
+function countdown() {  // Counts down to zero from a preset number
     var seconds = document.getElementById("seconds").value;
 
-    function tick() {
+    function tick() {  // Tick counts a range equally spaced
         seconds = seconds - 1;
         timer.innerHTML = seconds;
-        vartime = setTimeout(tick, 1000);
+        vartime = setTimeout(tick, 1000);  // setTimeout calls a function after a set number of milliseconds
         if (seconds == -1) {
             alert("Time is up!");
             clearTimeout(time);
@@ -13,3 +13,33 @@ function countdown() {
     }
     tick();
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);  // showSlides is a function that sets images
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
