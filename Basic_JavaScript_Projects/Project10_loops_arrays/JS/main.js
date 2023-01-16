3afd5402e0a7fa3219127b20e990c0412eacf168
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-16 12:21:09 -0700

    pi function
    Added pi function but not running

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -21,3 +21,39 @@ function for_Loop() {
     }
     document.getElementById("veg_garden").innerHTML = veggies;
 }
+
+function array_Function() {
+    var babyYoda = [];
+    babyYoda[0] = "looking cute";
+    babyYoda[1] = "looking cuter";
+    babyYoda[2] = "looking pensive";
+    babyYoda[3] = "looking adorable";
+    babyYoda[4] = "looking like a toddler";
+    babyYoda[5] = "looking sweet";
+    document.getElementById("Array").innerHTML = "In this picture, Baby Yoda is " + babyYoda[2] + ".";
+}
+
+function constant_function() {
+    const dog = {type:"labradoodle", weight: "30 lbs", color: "black"};
+    dog.color = "brown";
+    dog.price = "$2200";
+    const dog2 = {type:"goldendoodle", weight: "60 lbs", color: "red"};
+    dog2.price = "$2500";
+    document.getElementById("constant").innerHTML = "The cost of the " +
+    dog.type + " was " + dog.price + " and the cost of the " + dog2.type + " was " + dog2.price + " . ";
+}
+
+function varlet_function() {
+    var A = 100;
+    document.getElementById("varlet1").innerHTML = A;
+    {
+        let A = 200;
+        document.getElementById("varlet2").innerHTML = A;
+    }
+    document.getElementById("varlet3").innerHTML = A;
+}
+
+function pie_function() {
+    return Math.PI;
+}
+document.getElementById("Pie").innerHMTL = pie_function();
