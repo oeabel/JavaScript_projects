aefcb0191aee4ebd06768a1cea00483913d3c81e
oeabel <118934722+oeabel@users.noreply.github.com>
2023-01-17 15:54:29 -0700

    final tictactoe
    complete program. new sounds and images

--- TicTacToe/JS/tictactoe.js
+++ TicTacToe/JS/tictactoe.js
@@ -14,11 +14,11 @@ function placeXOrO(squareNumber) {
         //This condition checks who's turn it is
         if (activePlayer === 'X') {
             //If activePlayer is equal to 'X', the x.png is placed in HTML
-            select.style.backgroundImage = 'url("images/x.png")';
+            select.style.backgroundImage = 'url("images/new_x_resize.png")';
             //Active playermay only be 'X' or 'O', the o.png is placed in HTML
         } else {
             // If activePlayer is equal to 'O', the o.png is placed in HTML
-            select.style.backgroundImage = 'url("images/o.png")';
+            select.style.backgroundImage = 'url("images/new_o_resize.png")';
         }
         //squareNumber and activePlayer are concantenated together and added to array
         selectedSquares.push(squareNumber + activePlayer);
@@ -34,7 +34,7 @@ function placeXOrO(squareNumber) {
             activePlayer = 'X';
         }
         //This function plays placement sound
-        audio("./media/place.mp3");
+        audio("./media/bamboo_swing.mp3");
         //This condition checks to see if it is the computer's turn
         if (activePlayer === 'O') {
             //This function disables clicking for computer's turn
@@ -105,7 +105,7 @@ function checkWinConditions() {
     //9 squares are selected, the code executes.
     else if (selectedSquares.length >= 9) {
         //This function plays the tie game sound.
-        audio('./media/tie.mp3');
+        audio('./media/swish.mp3');
         //This function sets a .3 second timer before the resetGame is called.
         setTimeout(function() {resetGame(); }, 500);
     }
@@ -121,4 +121,101 @@ function arrayIncludes(squareA, squareB, squareC) {
     //true is returned and our else if condition executes the drawLine() function.
     if (a === true && b === true && c === true) {return true;}
     }   
+}
+
+//This function makes our body element temporarily unclickable
+function disableClick() {
+    //This makes our body unclickable
+    body.style.pointerEvents = 'none';
+    //This makes our body clickable again after 1 second
+    setTimeout(function() { body.style.pointerEvents = 'auto'; }, 1000);
+}
+
+//This function takes a string parameter of the path you set earlier for
+//placement sound('./media/place.mp3')
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    //Play method plays our audio sound
+    audio.play();
+}
+
+//This function utilizes the HTML canvas to draw win lines
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    //This line accesses our HTML canvas element
+    const canvas = document.getElementById('win-lines');
+    //This line gives us access to methods and properties to use on canvas
+    const c = canvas.getContext('2d');
+    //This line indicates where the start of a lines x asix is
+    let x1 = coordX1,
+    //This line indicates where the start of a lines y axis is
+    y1 = coordY1,
+    //This line indicates where the end of a lines x axis is
+    x2 = coordX2,
+    //This line indicxates where the ends of a lines y axis is
+    y2 = coordY2,
+    //This variable stores temporary x axis data we update in our animation loop
+    x = x1,
+    //This variable stores temporary y axis data we update in our animation loop
+    y = y1;
+
+    //This function interacts with canvas
+    function animateLineDrawing() {
+        //This variable creates a loop
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        //This method clears content from the last loop iteration
+        c.clearRect(0, 0, 608, 608);
+        //This method starts a new path
+        c.beginPath();
+        //This method moves us to a starting point in our line
+        c.moveTo(x1, y1);
+        //This method indicates the end point in our line
+        c.lineTo(x, y);
+        //This method sets the width of our line
+        c.lineWidth = 10;
+        //This method sets the color of our line
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        //This method draws everything we laid out above
+        c.stroke();
+        //This condition checks if we've reached the endpoints
+        if (x1 <= x2 && y1 <= y2) {
+            //This condition adds 10 to the previous end x endpoint
+            if (x < x2) { x += 10; }
+            //This condition adds 10 to the previous end y endpoint
+            if (y < y2) { y += 10; }
+            //This condition is similar to the one above
+            //This is necessary for the 6, 4, 2 win conditions
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+    //This function clears our canvas after our win line is drawn
+    function clear() {
+        //This line starts our animation loop
+        const animationLoop = requestAnimationFrame(clear);
+        //This line clears our canvas
+        c.clearRect(0, 0, 608, 608);
+        //This line stops our animation loop
+        cancelAnimationFrame(animationLoop);
+    }
+    //This line disallows clicking while the win sound is playing
+    disableClick();
+    //This line plays the win sounds
+    audio('./media/spin_swish.mp3');
+    //This line calls our main animation loop
+    animateLineDrawing();
+    //This line waits 1 second, then clears canvas, resets game, and allows clikcing again
+    setTimeout(function () { clear(); resetGame(); }, 1000);
+}
+
+//This function resets the game in the even of a tie or a win
+function resetGame() {
+    //This for loop iterates through each HTML square element
+    for (let i = 0; i < 9; i++) {
+        //This variable gets the HTML element i
+        let square = document.getElementById(String(i));
+        //This removes our elements backgroundImage
+        square.style.backgroundImage = '';
+    }
+    //This resets our array so that it is empty and we can start over
+    selectedSquares = [];
 }
